c5fa8f117d1ac4b98d6f3876be298894d0e6675c
CLauchanchisan <claudia_casalone@hotmail.com>
2025-09-11 21:45:52 -0300

    union carrusel y home

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
-
+import { CarruselComponent } from '../components/carrusel/carrusel.component';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule, CarruselComponent],
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
