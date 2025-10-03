86deb76d21c71187c5ccb5520205df064fb83df0
CLauchanchisan <claudia_casalone@hotmail.com>
2025-10-02 22:10:59 -0300

    boton que redirige a contacto

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-
+import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule,RouterLink],
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
--- src/app/components/nosotros/nosotros.ts
+++ src/app/components/nosotros/nosotros.ts
@@ -1,21 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-nosotros',
-  imports: [ CommonModule],
+  imports: [CommonModule],
   templateUrl: './nosotros.html',
   styleUrl: './nosotros.css'
 })
-export class Nosotros {
+export class Nosotros implements AfterViewInit {
   equipo1 = [
-    { nombre: 'Claudia', img: '/perrito2Nos.png'},
-    { nombre: 'Jean', img: '/perrito2Nos.png'},
-    { nombre: 'Sole', img: '/perrito2Nos.png'}
+    { nombre: 'Claudia', img: '/perrito2Nos.png' },
+    { nombre: 'Jean', img: '/perrito2Nos.png' },
+    { nombre: 'Sole', img: '/perrito2Nos.png' }
   ];
 
   equipo2 = [
-    { nombre: 'Sergio', img: '/perrito.png'},
-    { nombre: 'Edgar', img: '/perrito.png'},
-    { nombre: 'Juanjo', img: '/perrito.png'}
+    { nombre: 'Sergio', img: '/perrito.png' },
+    { nombre: 'Edgar', img: '/perrito.png' },
+    { nombre: 'Juanjo', img: '/perrito.png' }
   ];
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngAfterViewInit(): void {
+    this.route.fragment.subscribe(fragment => {
+      if (fragment) {
+        const element = document.getElementById(fragment);
+        if (element) {
+          // Scroll suave a la sección
+          element.scrollIntoView({ behavior: 'smooth' });
+        }
+      }
+    });
+  }
 }
