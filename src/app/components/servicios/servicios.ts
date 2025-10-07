8421d7438679e8f5ee543c371e03c84798925241
JJMDT <127325507+JJMDT@users.noreply.github.com>
2025-10-07 20:20:23 -0300

    mock de prueba

--- src/app/components/servicios/servicios.ts
+++ src/app/components/servicios/servicios.ts
@@ -1,29 +1,34 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Card } from '../../models/card.model';
 import { CardsService } from '../../services/cards.service';
+import { Producto } from '../../models/producto.model';
+import { listaProductos } from '../../mocks/productos.mock';
 
 @Component({
   selector: 'app-servicios',
+  standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './servicios.html',
   styleUrl: './servicios.css'
 })
-export class Servicios implements OnInit{
+export class Servicios implements OnInit {
   cards: Card[] = [];
+  productos: Producto[] = [];
   filtroTipo: string = '';
 
   constructor(private cardsService: CardsService) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.cardsService.getCards().subscribe(cards => this.cards = cards);
+    this.productos = listaProductos;
   }
 
   cardsFiltradas(): Card[] {
-  if (!this.filtroTipo) {
-    return this.cards; // devuelve array
+    if (!this.filtroTipo) {
+      return this.cards;
+    }
+    return this.cards.filter((card: Card) => card.tipo === this.filtroTipo);
   }
-  return this.cards.filter((card: Card) => card.tipo === this.filtroTipo); // devuelve array filtrado
-}
 }
---
+++ src/app/models/producto.model.ts
@@ -0,0 +1,8 @@
+export interface Producto {
+    id: number,
+    nombre: string,
+    categoria: string,
+    precio: number,
+    descripcion?: string
+    img: string
+}
