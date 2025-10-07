29c7f3c1901113f67bee6126bf4f23bab2599617
jeanbrcho <jeancbs18@gmail.com>
2025-10-07 16:30:10 -0300

    se creo el componente registro-usuario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import { Nosotros } from './components/nosotros/nosotros';
+import { RegistroUsuario } from './components/registro-usuario/registro-usuario';
 
 
 export const routes: Routes = [
    { path: '', component: Home },
-   { path:'nosotros', component: Nosotros}
+   { path:'nosotros', component: Nosotros},
+   { path:'registro', component: RegistroUsuario}
 ];
 
