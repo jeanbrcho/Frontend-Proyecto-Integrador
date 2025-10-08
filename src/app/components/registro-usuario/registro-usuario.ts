14b73146f4678f4a2dd3336a612bc2fff0eb4d04
zolemello <mello.soledad@gmail.com>
2025-10-07 23:12:29 -0300

    login update

--- src/app/components/registro-usuario/registro-usuario.ts
+++ src/app/components/registro-usuario/registro-usuario.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-registro-usuario',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './registro-usuario.html',
   styleUrl: './registro-usuario.css'
 })
