2808888774b3e518d4fb6dbc508d313fb80a77d3
zolemello <mello.soledad@gmail.com>
2025-10-07 23:06:19 -0300

    login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import { Nosotros } from './components/nosotros/nosotros';
 import { RegistroUsuario } from './components/registro-usuario/registro-usuario';
+import { LoginUsuario } from './components/login-usuario/login-usuario';
 
 
 export const routes: Routes = [
    { path: '', component: Home },
    { path:'nosotros', component: Nosotros},
-   { path:'registro', component: RegistroUsuario}
+   { path:'registro', component: RegistroUsuario},
+   { path:'login', component: LoginUsuario}
 ];
 
