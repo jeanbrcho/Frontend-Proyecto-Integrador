b844321d4d1d8b68470cb15ceaf50057eb77bda2
zolemello <mello.soledad@gmail.com>
2025-10-23 20:58:04 -0300

    conexion login terminado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { RegistroUsuario } from './components/registro-usuario/registro-usuario'
 import { LoginUsuario } from './components/login-usuario/login-usuario';
 import { Servicios } from './components/servicios/servicios';
 import { TurnoComponent } from './components/turno/turno.component';
+import { MiCuentaComponent } from './components/mi-cuenta/mi-cuenta.component';
 
 export const routes: Routes = [
    { path: '', component: Home },
@@ -12,6 +13,7 @@ export const routes: Routes = [
    { path:'registro', component: RegistroUsuario},
    { path:'login', component: LoginUsuario},
    { path: 'servicios', component: Servicios},
-   { path: 'turno', component: TurnoComponent }
+   { path: 'turno', component: TurnoComponent },
+   { path: 'mi-cuenta', component: MiCuentaComponent }
 ];
 
