e665978d885809a72a2aa8d971ceb638995e6eb1
CLauchanchisan <claudia_casalone@hotmail.com>
2025-10-23 22:36:01 -0300

    Ordenar Carpetas

---
+++ src/app/service/cards.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Card } from '../interfaces/card.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CardsService {
+
+  private apiUrl = 'http://localhost:3005/cards';
+
+  constructor(private http: HttpClient) { }
+
+  getCards(): Observable<Card[]> {
+    return this.http.get<Card[]>(this.apiUrl);
+  }
+}
